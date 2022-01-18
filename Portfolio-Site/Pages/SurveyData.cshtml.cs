91080b89369abdfcc147f973d264bb6c2d67bcca
MindSet-design <nicnow007@gmail.com>
2022-01-17 20:22:28 -0700

    Changed to httpclient and c# in html

--- Portfolio-Site/Pages/SurveyData.cshtml.cs
+++ Portfolio-Site/Pages/SurveyData.cshtml.cs
@@ -1,14 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortfolioSite.Models;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace PortfolioSite.Pages
 {
     public class SurveyDataModel : PageModel
     {
+        private HttpClient _client;
 
-        public void OnGet()
+        public SurveyDataModel(HttpClient client)
         {
+            _client = client;
+        }
 
+        public List<Survey> Surveys { get; set; }
+        public async  Task<IActionResult> OnGetAsync()
+        {
+            var response = await _client.GetAsync("http://localhost:5193/api/survey");
+            var result = await response.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<List<Survey>>(result);
+            Surveys = model;
+            return Page();
         }
 
 
